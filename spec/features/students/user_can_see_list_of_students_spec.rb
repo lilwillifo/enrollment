6025d7eea6b4a8a90efb2cd39c190b1d81c5c601
lilwillifo <margaretwilliford4@gmail.com>
2018-03-27 14:18:08 -0600

    index links to show for each student

--- spec/features/students/user_can_see_list_of_students_spec.rb
+++ spec/features/students/user_can_see_list_of_students_spec.rb
@@ -15,5 +15,17 @@ describe 'As a user' do
       expect(page).to have_content(student_3.name)
       expect(page).to have_content(student_4.name)
     end
+    it 'I can link to a specific student' do
+      student_1 = Student.create!(name: 'Margaret')
+      student_2 = Student.create!(name: 'Haley')
+      student_3 = Student.create!(name: 'Adam')
+      student_4 = Student.create!(name: 'Andrew')
+
+      visit '/students'
+      click_on "#{student_1.name}"
+
+      expect(current_path).to eq(student_path(student_1))
+      expect(page).to have_content(student_1.name)
+    end
   end
 end
