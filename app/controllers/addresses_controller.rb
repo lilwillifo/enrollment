6274ac1b157d294255b28f7973867c606ba3b1b7
lilwillifo <margaretwilliford4@gmail.com>
2018-03-27 14:59:29 -0600

    WIP

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,4 +3,18 @@ class AddressesController < ApplicationController
     @student = Student.find(params[:student_id])
     @address = Address.new
   end
+
+  def create
+    @student = Student.find(params[:student_id])
+    @address = Address.new(address_params)
+    @address.save
+
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:description, :street, :city, :state, :zip, :student_id)
+  end
 end
